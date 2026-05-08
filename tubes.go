268659e4a95528f262d36097e276c118fac249d0
jefmrd <jefrijabil@gmail.com>
2026-05-08 21:36:04 +0700

    memperbaiki menu tambah dengan menambahkan fitur masukan beberapa menu sekaligus dan membuat hapusMenu

--- tubes.go
+++ tubes.go
@@ -1,7 +1,5 @@
 package main
 import "fmt"
-
-
 const NMAX int = 999
 type dataMenu [NMAX] menu
 type menu struct {
@@ -13,31 +11,26 @@ type menu struct {
 
 func main(){
 	var listMenu dataMenu
-	var tambahNamaMenu, tambahKategori, tambahKomposisiMenu string
-	var tambahHargaMenu int
 	var menuUtama, menuAksi int
+	var jumlahMenu int
 	listMenu = dataMenu{
-		{nama: "Nasi Goreng", 
+		{nama: "Nasi_Goreng", 
 		harga: 15000, 
 		komposisi: "Nasi, Telur, Kecap",
 		kategori: "makanan utama",
 		},
-		{nama:      "Matcha Latte",
+		{nama:      "Matcha_Latte",
         harga:     18000,
         komposisi: "Bubuk Matcha, Susu Segar",
         kategori:  "minuman",
 		},
-		{nama:      "Kentang Goreng",
+		{nama:      "Kentang_Goreng",
         harga:     12000,
         komposisi: "Kentang, Garam, Kaldu",
         kategori:  "makanan ringan",
 		},
 	}
 	
-	tambahNamaMenu = ""
-	tambahHargaMenu = 0
-	tambahKomposisiMenu = ""
-	tambahKategori = ""
 	menuUtama = 1
 	fmt.Println("Selamat datang diCafe-menu (^o^)")
 	for menuUtama == 1 {
@@ -46,26 +39,27 @@ func main(){
 		fmt.Println("1.Menampilkan menu")
 		fmt.Println("2.Menambahkan menu")
 		fmt.Println("3.Mengubah menu")
-		fmt.Println("4.Menangurangi menu")
+		fmt.Println("4.Mengurangi menu")
 		fmt.Scan(&menuAksi)
 		if menuAksi == 1 {
 			fmt.Println("Menampilkan menu yang tersedia")
 			tampilMenu(listMenu)
 		}else if menuAksi == 2 {
+			fmt.Println("Masukkan jumlah menu yang ingin ditambahkan")
+			fmt.Scan(&jumlahMenu)
 			fmt.Println("Masukkan menu yang ingin anda tambahkan (tanpa spasi atau gunakan _ )")
-			fmt.Print("Nama menu: ")
-			fmt.Scan(&tambahNamaMenu)
-			fmt.Print("Harga menu: Rp")
-			fmt.Scan(&tambahHargaMenu)
-			fmt.Print("Komposisi menu: ")
-			fmt.Scan(&tambahKomposisiMenu)
-			fmt.Print("Kategori menu: ")
-			fmt.Scan(&tambahKategori)
-			tambahMenu(tambahNamaMenu, tambahKomposisiMenu, tambahKategori, tambahHargaMenu, &listMenu)
+			fmt.Println("*Note: Kategori diisi dengan coffe atau noncoffe")
+			for i:=1; i<=jumlahMenu;i++{
+				tambahMenu(&listMenu)
+			}
+			tampilMenu(listMenu)
 		}else if menuAksi == 3 {
-			fmt.Println("")
+			fmt.Println("Pilih menu nomor berapa yang ingin anda ubah")
+			
 		}else if menuAksi == 4 {
-			fmt.Println("")
+			fmt.Println("Pilih menu yang ingin anda hilangkan")
+			tampilMenu(listMenu)
+			hapusMenu(&listMenu)
 		}else {
 			fmt.Println("Aksi tidak tersedia (!_!)")
 		}
@@ -93,9 +87,19 @@ func tampilMenu(listMenu dataMenu){
 	fmt.Println("------------------------------------------------------------------------------------------------")
 }
 
-func tambahMenu (nama, komposisi, kategori string, harga int, listMenu*dataMenu){
+func tambahMenu (listMenu*dataMenu){
 	var found int
-	
+	var nama, komposisi, kategori string
+	var harga int
+	fmt.Print("Nama menu: ")
+	fmt.Scan(&nama)
+	fmt.Print("Harga menu: Rp")
+	fmt.Scan(&harga)
+	fmt.Print("Komposisi menu: ")
+	fmt.Scan(&komposisi)
+	fmt.Print("Kategori menu: ")
+	fmt.Scan(&kategori)
+	fmt.Println("")
 	found = -1
 	i := 0
 	for found == -1 {
@@ -107,6 +111,49 @@ func tambahMenu (nama, komposisi, kategori string, harga int, listMenu*dataMenu)
 			found = 1
 		}
 		i++
+	}	
+}
+
+func hapusMenu(listMenu*dataMenu){
+	var namaMenu string
+	var idxMenu int
+	
+	fmt.Print("Nama menu: ")
+	fmt.Scan(&namaMenu)
+	
+	if namaMenu == "67" {
+		idxMenu = -2
+	}else {
+		idxMenu = cariMenuDenganSequential(namaMenu, *listMenu)
 	}
 	
+	if idxMenu == -1 {
+		fmt.Println("Data menu tidak tersedia.\nSilahkan masukkan menu kembali atau ketik 67 untuk berhenti")
+		hapusMenu(listMenu)
+	}else if idxMenu == -2 {
+		fmt.Println("Kembali ke menu utama")
+	}else {
+		for listMenu[idxMenu].nama != "" {
+			listMenu[idxMenu].nama = listMenu[idxMenu + 1].nama
+			listMenu[idxMenu].harga = listMenu[idxMenu+1].harga
+			listMenu[idxMenu].komposisi = listMenu[idxMenu+1].komposisi
+			listMenu[idxMenu].kategori = listMenu[idxMenu+1].kategori
+			idxMenu++
+		}
+		tampilMenu(*listMenu)
+	}
+	
+}
+
+func cariMenuDenganSequential (nama string, listMenu dataMenu)int{
+	var i, found int
+	i = 0
+	found = -1
+	for found == -1 && listMenu[i].nama != "" {
+		if nama == listMenu[i].nama {
+			found = i
+		}
+		i++
+	}
+	return found
 }
